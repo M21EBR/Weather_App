3cb01bc5bd2ac9892c6ce62e2a76ed5a217748b3
Anni_13 <anirudhar313@gmail.com>
2025-08-19 15:42:09 +0530

    final changes in Feature branch for backend

--- weather_app/src/main/java/com/example/weather_app/Controller/WeatherController.java
+++ weather_app/src/main/java/com/example/weather_app/Controller/WeatherController.java
@@ -1,13 +1,39 @@
 package com.example.weather_app.Controller;
 
+import com.example.weather_app.model.WeatherResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
+@Controller
 public class WeatherController {
 
+    @Value("${api.key}")
+    private String apikey;
+
     @GetMapping("/")
-    public String hello(){
-        return "hello world";
+    public String getIndex(){
+        return "Index";
+    }
+
+    @GetMapping("/weather")
+    public String city(@RequestParam String city, Model model){
+        String url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey+"&units=metric";
+        RestTemplate rt = new RestTemplate();
+        WeatherResponse wr = rt.getForObject(url,WeatherResponse.class);
+
+        if(wr != null){
+            model.addAttribute("city",wr.getName());
+            model.addAttribute("country",wr.getSys().getCountry());
+            model.addAttribute("weatherDescription",wr.getWeather().get(0).getDescription());
+            model.addAttribute("temperature",wr.getMain().getTemp());
+            model.addAttribute("humidity",wr.getMain().getHumidity());
+            model.addAttribute("windSpeed",wr.getWind().getSpeed());
+        }
+        return "weather";
     }
 }
--- weather_app/src/main/java/com/example/weather_app/model/WeatherResponse.java
+++ weather_app/src/main/java/com/example/weather_app/model/WeatherResponse.java
@@ -1,4 +1,41 @@
-package com.example.weather_app.Model;
+package com.example.weather_app.model;
 
+import lombok.Data;
+
+import java.util.*;
+
+@Data
 public class WeatherResponse {
+
+    private String name;
+
+    private  Sys sys;
+
+    private List<Weather> weather;
+
+    private Main main;
+
+    private Wind wind;
+
+    @Data
+    public static class Sys{
+        private String country;
+    }
+
+    @Data
+    public static class Weather{
+        private int id;
+        private String description;
+    }
+
+    @Data
+    public static class Main{
+        private double temp;
+        private int humidity;
+    }
+
+    @Data
+    public static class Wind{
+        private double speed;
+    }
 }
